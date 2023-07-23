b325759cf1b5c6bdb06968a5a296c4d08f0d7af2
kesav <kesavyerra@yahoo.in>
2023-07-23 00:49:00 -0400

    Added First Project with Day 1 Example

---
+++ src/main/java/day2/CalaculateDiscount.java
@@ -0,0 +1,22 @@
+package day2;
+
+import java.util.Scanner;
+
+public class CalaculateDiscount {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter thr Bill amount: ");
+        double billAmount = sc.nextDouble();
+
+        double discount = 0;
+        if(billAmount >= 1000){
+            discount = billAmount * 0.1;
+        }
+        double netAmount = billAmount - discount;
+        System.out.println("Bill Amount :"+billAmount);
+        System.out.println("Discount :"+discount);
+        System.out.println("Net Amount :"+netAmount);
+
+
+    }
+}
---
+++ src/main/java/day2/CalculateDiscountWithElse.java
@@ -0,0 +1,28 @@
+package day2;
+
+import java.util.Scanner;
+
+public class CalculateDiscountWithElse {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the bill amount: ");
+        double billAmount =sc.nextDouble();
+
+        double discount;
+
+        if(billAmount >= 1000) {
+            discount = billAmount * 0.1;
+            double netAmount = billAmount - discount;
+        }
+        else {
+            discount = billAmount * 0.05;
+            double netAmount = billAmount - discount;
+        }
+        double netAmount = billAmount - discount;
+        System.out.println("Bill Amount :"+billAmount);
+        System.out.println("Discount :"+discount);
+        System.out.println("Net Amount :"+netAmount);
+
+    }
+}
