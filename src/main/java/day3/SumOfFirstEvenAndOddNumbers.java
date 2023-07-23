cf7e62f7aaf37da28d7bfab790218deab94f3aee
kesav <kesavyerra@yahoo.in>
2023-07-23 16:33:45 -0400

    Added First Project with Day 1 Example

---
+++ src/main/java/day3/SumOfFirstEvenAndOddNumbers.java
@@ -0,0 +1,25 @@
+package day3;
+
+import java.util.Scanner;
+
+public class SumOfFirstEvenAndOddNumbers {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter the number : ");
+        int num = sc.nextInt();
+        int evensum = 0;
+        int oddsum = 0;
+        int sum =0 ;
+        for(int i=1; i<=num; i++){
+            sum +=i;
+            if(i % 2==0){
+                evensum +=i;
+            }else{
+                oddsum +=i;
+            }
+        }
+        System.out.println("Sum of first "+num+" numbers is "+sum);
+        System.out.println("Sum of first "+num+" even numbers is "+evensum);
+        System.out.println("Sum of first "+num+" odd numbers is "+oddsum);
+    }
+}
